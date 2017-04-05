5d76d40ddaec0315d0a9b41d47f8e0b314c9e386
kdrphoto <konrad.gustawski@gmail.com>
2017-04-05 10:44:18 +0200

    Add project files.

---
+++ Aplikacja1/Figury/hetman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja1.figury
+{
+    class hetman : figura
+    {
+        public hetman(Pozycja pozycja, string kolor) : base (pozycja,kolor)
+        {
+
+        }
+        public virtual bool CzyMoznaPrzesunac(Pozycja nowaPozycja)
+        {
+            return CzyPionowo(nowaPozycja) || CzyPoziomo(nowaPozycja) || CzySkos(nowaPozycja);
+        }
+    }
+
+}
---
+++ Aplikacja1/Figury/kolor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja1.figury
+{
+    class folor
+    {
+        public static string KOLOR_BIALY = "Bialy";
+        public static string KOLOR_CZARNY = "Czarny";
+    }
+}
